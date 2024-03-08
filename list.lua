494603dde5c4417bcdb04a8fc321567649472ab5
Cei <123380487+CeiCode@users.noreply.github.com>
2024-03-08 16:29:54 +0100

    Update list.lua

--- list.lua
+++ list.lua
@@ -2,7 +2,7 @@ local _, t = ...
 
 -- Crusader Strike Discord Blocklist Table
 
--- unique characters: 81
+-- unique characters: 86
 
 t.version = "@project-version@"
 
@@ -65,55 +65,62 @@ t.case_table = {
 },
 
 [9] = {
+    name = "Borngoòd",
+    faction = "Alliance",
+    description = "Scamming - Deathroll",
+    url = "https://discord.com/channels/1177750568433881138/1214188633930731551",
+},
+
+[10] = {
     name = "Chopperheal",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1209186802712318032",
 },
 
-[10] = {
+[11] = {
     name = "Cococanel",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1209584001392844800",
 },
 
-[11] = {
+[12] = {
     name = "Cryspi",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1208497873851908126",
 },
 
-[12] = {
+[13] = {
     name = "Ðeadly",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1209562915401760820",
 },
 
-[13] = {
+[14] = {
     name = "Díng",
     faction = "Alliance",
     description = "Scamming - Deathroll",
     url = "https://discord.com/channels/1177750568433881138/1208917113671258122",
 },
 
-[14] = {
+[15] = {
     name = "Duruid",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1197096906883604511",
 },
 
-[15] = {
+[16] = {
     name = "Elonprime ",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1208792343290060850",
 },
 
-[16] = {
+[17] = {
     name = "Fatherxmas",
     faction = "Alliance",
     guid = "Player-5828-01D7AEEF",
@@ -121,56 +128,56 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1207449636478914570",
 },
 
-[17] = {
+[18] = {
     name = "Flaffyz",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1197106764110377101",
 },
 
-[18] = {
+[19] = {
     name = "Flamebringer",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1212144052737286244",
 },
 
-[19] = {
+[20] = {
     name = "Froozy",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1208499053890117662",
 },
 
-[20] = {
+[21] = {
     name = "Frostyboi",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1197153924914892820",
 },
 
-[21] = {
+[22] = {
     name = "Gompie",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1209584793067585597",
 },
 
-[22] = {
+[23] = {
     name = "Hardyka",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1201588306161447003",
 },
 
-[23] = {
+[24] = {
     name = "Hargon",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1197684926514991134",
 },
 
-[24] = {
+[25] = {
     name = "Huntersmi",
     faction = "Alliance",
     guid = "Player-5828-01FE5AA4",
@@ -178,14 +185,14 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1207450897114857502",
 },
 
-[25] = {
+[26] = {
     name = "Icelancex",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1208878988714311751",
 },
 
-[26] = {
+[27] = {
     name = "Jens",
     faction = "Alliance",
     guid = "Player-5828-01CD1BA7",
@@ -193,7 +200,7 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1208476948104482876",
 },
 
-[27] = {
+[28] = {
     name = "Kimmortal",
     faction = "Alliance",
     guid = "Player-5828-021A8E09",
@@ -201,28 +208,28 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1206001103930396722",
 },
 
-[28] = {
+[29] = {
     name = "Krokoval",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1209089366400569366",
 },
 
-[29] = {
+[30] = {
     name = "Kruczq",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1208073198013513788",
 },
 
-[30] = {
+[31] = {
     name = "Kshatriya",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1199187425143042131",
 },
 
-[31] = {
+[32] = {
     name = "Lanosu",
     faction = "Alliance",
     guid = "Player-5828-01CA76DB",
@@ -230,7 +237,7 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1207872998791450674",
 },
 
-[32] = {
+[33] = {
     name = "Lilkahi",
     faction = "Alliance",
     guid = "Player-5828-01E50480",
@@ -238,35 +245,35 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1208072155028262932",
 },
 
-[33] = {
+[34] = {
     name = "Lilmama",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1204729282753069097",
 },
 
-[34] = {
+[35] = {
     name = "Locku",
     faction = "Alliance",
     description = "Scamming - Deathroll",
     url = "https://discord.com/channels/1177750568433881138/1208463478759755918",
 },
 
-[35] = {
+[36] = {
     name = "Malmiz",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1204485379420328047",
 },
 
-[36] = {
+[37] = {
     name = "Marioparty ",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1208766393177210901",
 },
 
-[37] = {
+[38] = {
     name = "Metlar",
     faction = "Alliance",
     guid = "Player-5828-021559CC",
@@ -274,49 +281,50 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1203856001108021258",
 },
 
-[38] = {
+[39] = {
     name = "Munters",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1203094313425174570",
 },
 
-[39] = {
-    name = "Niylh ",
+[40] = {
+    name = "Omarsy",
     faction = "Alliance",
-    description = "Ninja Looting - Raid",
-    url = "https://discord.com/channels/1177750568433881138/1209176037129322557",
+    guid = "Player-5828-01CC2959",
+    description = "Scamming - Summon",
+    url = "https://discord.com/channels/1177750568433881138/1215409370549063791",
 },
 
-[40] = {
+[41] = {
     name = "Palkia",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1211850152164462662",
 },
 
-[41] = {
+[42] = {
     name = "Palkinsons",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1212345865759625228",
 },
 
-[42] = {
+[43] = {
     name = "Ploto",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1210430229668036649",
 },
 
-[43] = {
+[44] = {
     name = "Popcorny",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1206085005458014229",
 },
 
-[44] = {
+[45] = {
     name = "Pubbasubba",
     faction = "Alliance",
     guid = "Player-5828-01FB0D18",
@@ -324,42 +332,42 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1200894090909319299",
 },
 
-[45] = {
+[46] = {
     name = "Reav",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1210296683481800784",
 },
 
-[46] = {
+[47] = {
     name = "Remoria",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1209603292276195358",
 },
 
-[47] = {
+[48] = {
     name = "Scùmbag",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1208029514324770856",
 },
 
-[48] = {
+[49] = {
     name = "Sigeric",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1207871036939444274",
 },
 
-[49] = {
+[50] = {
     name = "Silatkele",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1209823938939584572",
 },
 
-[50] = {
+[51] = {
     name = "Sonate",
     faction = "Alliance",
     guid = "Player-5828-021B2CFA",
@@ -367,26 +375,19 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1202739491660632136",
 },
 
-[51] = {
+[52] = {
     name = "Starfoula",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1199476207062896810",
 },
 
-[52] = {
+[53] = {
     name = "Synelia",
     faction = "Alliance",
     guid = "Player-5828-01E5CAC7",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1207308942401736734",
-},
-
-[53] = {
-    name = "Szarvas",
-    faction = "Alliance",
-    description = "Scamming - Summon",
-    url = "https://discord.com/channels/1177750568433881138/1211738143901089843",
 },
 
 [54] = {
@@ -412,20 +413,27 @@ t.case_table = {
 },
 
 [57] = {
+    name = "Thorïn",
+    faction = "Alliance",
+    description = "Scamming - Summon",
+    url = "https://discord.com/channels/1177750568433881138/1214746511078981632",
+},
+
+[58] = {
     name = "Tollak",
     faction = "Alliance",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1201588306161447003",
 },
 
-[58] = {
+[59] = {
     name = "Tomyamgoong",
     faction = "Alliance",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1207872143031803934",
 },
 
-[59] = {
+[60] = {
     name = "Touffu",
     faction = "Alliance",
     guid = "Player-5828-0214B437",
@@ -433,14 +441,14 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1200906903509803038",
 },
 
-[60] = {
+[61] = {
     name = "Wilsonfisk",
     faction = "Alliance",
     description = "Scamming - Deathroll",
     url = "https://discord.com/channels/1177750568433881138/1197577021555998751",
 },
 
-[61] = {
+[62] = {
     name = "Winstonblue",
     faction = "Alliance",
     guid = "Player-5828-021A8FB1",
@@ -448,7 +456,7 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1201330373695643778",
 },
 
-[62] = {
+[63] = {
     name = "Billypaladin",
     faction = "Horde",
     guid = "Player-5828-01DEC3D9",
@@ -456,28 +464,42 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1205623287460266096",
 },
 
-[63] = {
+[64] = {
     name = "Bilypaladin",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1201504162270162964",
 },
 
-[64] = {
+[65] = {
+    name = "Cannuh ",
+    faction = "Horde",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1177750568433881138/1215434761858846790",
+},
+
+[66] = {
+    name = "Chalaa",
+    faction = "Horde",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1177750568433881138/1215434761858846790",
+},
+
+[67] = {
     name = "Cowbear",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1209982912305561730",
 },
 
-[65] = {
+[68] = {
     name = "Dexty",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1208030182250643557",
 },
 
-[66] = {
+[69] = {
     name = "Farligko",
     faction = "Horde",
     guid = "Player-5828-01FB1C1D",
@@ -485,21 +507,21 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1202764815387467776",
 },
 
-[67] = {
+[70] = {
     name = "Flabala",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1205898575381266472",
 },
 
-[68] = {
+[71] = {
     name = "Frostkeeper",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1203747879936594051",
 },
 
-[69] = {
+[72] = {
     name = "Huntersocka",
     faction = "Horde",
     guid = "Player-5828-02130565",
@@ -507,21 +529,21 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1202764815387467776",
 },
 
-[70] = {
+[73] = {
     name = "Jimbola",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1203737031759372308",
 },
 
-[71] = {
+[74] = {
     name = "Kelzuê",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1203747879936594051",
 },
 
-[72] = {
+[75] = {
     name = "Kisco",
     faction = "Horde",
     guid = "Player-5828-0200FC7A",
@@ -529,7 +551,7 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1203862523963510926",
 },
 
-[73] = {
+[76] = {
     name = "Lawabider",
     faction = "Horde",
     guid = "Player-5828-01CD98C2",
@@ -537,56 +559,72 @@ t.case_table = {
     url = "https://discord.com/channels/1177750568433881138/1202764815387467776",
 },
 
-[74] = {
+[77] = {
     name = "Mayambela",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1198294227604492448",
 },
 
-[75] = {
+[78] = {
     name = "Nahmon",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1210370924709281802",
 },
 
-[76] = {
+[79] = {
     name = "Nekkface",
     faction = "Horde",
+    guid = "Player-5828-01E1E338",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1210541493471674368",
 },
 
-[77] = {
+[80] = {
     name = "Paplan",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1197564226953297940",
 },
 
-[78] = {
+[81] = {
     name = "Pumparumpa",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1207120200848248852",
 },
 
-[79] = {
+[82] = {
+    name = "Ragnosh",
+    faction = "Horde",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1177750568433881138/1215039545524097024",
+},
+
+[83] = {
     name = "Raschor",
     faction = "Horde",
+    guid = "Player-5828-0214EB76",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1204143937871282196",
 },
 
-[80] = {
+[84] = {
     name = "Sjintao",
     faction = "Horde",
     description = "Ninja Looting - Raid",
     url = "https://discord.com/channels/1177750568433881138/1205790790635491378",
 },
 
-[81] = {
+[85] = {
+    name = "Snusmonkey",
+    faction = "Horde",
+    description = "Ninja Looting - Raid",
+    url = "https://discord.com/channels/1177750568433881138/1213555481117990933",
+},
+
+[86] = {
     name = "Soz",
     faction = "Horde",
     description = "Ninja Looting - Raid",
