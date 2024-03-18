5e94d1553e51fbcb46a6323a23ed06367ac596d0
Spankwow <159571424+Spankwow@users.noreply.github.com>
2024-03-18 01:58:03 +0000

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -5,21 +5,21 @@ local _, t = ...
 
 -- Set this to be your list's name. If it doesn't have one, just set it to be the 
 -- same as your provider name.
-t.my_name = "Crusader Strike EU Blacklist"
+t.my_name = "Wildgrowth EU Blacklist"
 
 -- Set this variable to the name of your organisation, usually the name of your
 -- Discord.
 -- e.g. "Golemagg Discord"
-t.my_provider = "Crusader Strike EU discord"
+t.my_provider = "Wildgrowth EU discord"
 
 -- Set this variable to whatever realm your list is for.
 -- e.g. Golemagg, Firemaw, Gehennas etc.
-t.my_realm = "Crusader Strike"
+t.my_realm = "Wildgrowth"
 
 -- Set this variable to provide a description of your organisation.
 -- e.g. "Realm discord for the Golemagg EU server."
-t.my_description = "Blacklist curated by the Crusader Strike EU discord."
+t.my_description = "Blacklist curated by the Wildgrowth EU discord."
 
 -- Set this variable to be a url to your discord or website where people
 -- can come and find you.
-t.my_url = "https://discord.gg/dsxYHXFucH"
+t.my_url = "https://discord.gg/A63kMKaCk6"
