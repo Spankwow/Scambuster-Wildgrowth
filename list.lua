c5c504723d67fcba770448af852acb46cfa65f50
Spankwow <159571424+Spankwow@users.noreply.github.com>
2024-03-18 12:30:16 +0000

    Update list.lua

--- list.lua
+++ list.lua
@@ -2,17 +2,185 @@ local _, t = ...
 
 -- Wildgrowth Discord Blocklist Table
 
--- unique characters: 1
+-- unique characters: 22
 
-t.version = "@project-version@"
+t.version = "1.01"
 
 t.case_table = {
-[1] = {
-    name = "Wwenjoyer",
-    faction = "Horde",
-    guid = "Player-5825-02643286",
-    description = "Scamming - Summon",
-    url = "https://discord.com/channels/1177750568433881138/1210568673975341066",
+    [1] = {
+        name = "Withen",
+        faction = "Alliance",
+        guid = "Player-5825-020ABE2F",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1177750568433881138/1210568673975341066",
+    },
+    
+    [2] = {
+        name = "Rockeyshield",
+        faction = "Alliance",
+        guid = "Player-5825-020AC021",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1205241902774091786",
+    },
+    
+    [3] = {
+        name = "Foxlingarc",
+        faction = "Alliance",
+        guid = "Player-5825-020ABDAA",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1205241902774091786",
+    },
+    
+    [4] = {
+        name = "Zwippe",
+        faction = "Alliance",
+        guid = "Player-5825-024444B6",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1205254371420536833",
+    },
+    
+    [5] = {
+        name = "Kagenokishi",
+        faction = "Alliance",
+        guid = "Player-5825-0236E049",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1206847193877712906",
+    },
+    
+    [6] = {
+        name = "Ainssa",
+        faction = "Alliance",
+        guid = "Player-5825-021A7D26",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1206847193877712906",
+    },
+    
+    [7] = {
+        name = "Malpacippa",
+        faction = "Alliance",
+        guid = "Player 5825-02141DD0",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1207014436636860479",
+    },
+    
+    [8] = {
+        name = "Pasobtw",
+        faction = "Alliance",
+        guid = "Player-5825-021E3F1E",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1208609048971444276",
+    },
+    
+    [9] = {
+        name = "Dfgesrtysert",
+        faction = "Alliance",
+        guid = "Player-5825-023DFA43",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1209806427015745586",
+    },
+    
+    [10] = {
+        name = "Theryaklis",
+        faction = "Alliance",
+        guid = "Player-5825-023E10B6",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1212068615063474186",
+    },
+    
+    [11] = {
+        name = "Blackpinked",
+        faction = "Alliance",
+        guid = "Player-5825-022E527C",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1212854023296917584",
+    },
+    
+    [12] = {
+        name = "Mariobasler",
+        faction = "Alliance",
+        guid = "Player-5825-0205BA3E",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1212854023296917584",
+    },
+    
+    [13] = {
+        name = "Ferbruxo",
+        faction = "Alliance",
+        guid = "Player-5825-024B4B1F",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1216068847388328017",
+    },
+    
+    [14] = {
+        name = "Magìerin",
+        faction = "Alliance",
+        guid = "Player-5828-01E9631F",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1216146875040661535",
+    },
+    
+    [15] = {
+        name = "Förstahjälpn",
+        faction = "Horde",
+        guid = "Player-5825-02170EFE",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1205252314697437234",
+    },
+    
+    [16] = {
+        name = "Ibetrollinmn",
+        faction = "Horde",
+        guid = "Player-5825-02426749",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1205242216180752404",
+    },
+    
+    [17] = {
+        name = "Guldanita",
+        faction = "Horde",
+        guid = "Player-5825-021FCC34",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1207271289136357407",
+    },
+    
+    [18] = {
+        name = "Daasd",
+        faction = "Alliance",
+        guid = "Player-5825-0221C46B",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1209839575300575332",
+    },
+    
+    [19] = {
+        name = "Kazgrath",
+        faction = "Horde",
+        guid = "Player-5825-0252562C",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1208921883412660365",
+    },
+    
+    [20] = {
+        name = "Schnapsídee",
+        faction = "Horde",
+        guid = "Player-5825-023F8D74",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1211259155743117352",
+    },
+    
+    [21] = {
+        name = "Underøath",
+        faction = "Horde",
+        guid = "Player-5825-02500B8C",
+        description = "Harassment - Social Conduct",
+        url = "https://discord.com/channels/1170454697002422374/1218190217660465192",
+    },
+    
+    [22] = {
+        name = "Pàye",
+        faction = "Horde",
+        guid = "Player5825-021DC319",
+        description = "Ninja Looting - Raid",
+        url = "https://discord.com/channels/1170454697002422374/1217423435291168779",
 },
 
 }
