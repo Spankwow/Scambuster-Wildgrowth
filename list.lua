6884c404a6e8547d825aa21d8286154fb22c1806
Spankwow <159571424+Spankwow@users.noreply.github.com>
2024-03-18 02:48:20 +0000

    Update list.lua

--- list.lua
+++ list.lua
@@ -10,10 +10,9 @@ t.case_table = {
 [1] = {
     name = "Wwenjoyer",
     faction = "Horde",
-    guid = "Player-5825-02643286"
+    guid = "Player-5825-02643286",
     description = "Scamming - Summon",
     url = "https://discord.com/channels/1177750568433881138/1210568673975341066",
 },
 
 }
-
